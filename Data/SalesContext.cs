dff37012946cd6d6afdb84e9691ab3c5e441d04f
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-24 15:34:47 -0300

    Adicionado Scafolding de Departments

---
+++ Data/SalesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sales_ASPNET_Core.Models;
+
+namespace Sales_ASPNET_Core.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext (DbContextOptions<SalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sales_ASPNET_Core.Models.Department> Department { get; set; }
+    }
+}
