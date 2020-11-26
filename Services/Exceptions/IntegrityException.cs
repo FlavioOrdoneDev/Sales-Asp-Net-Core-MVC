edd62fa27489ba7266bba8c5a235e7024e62c48c
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-26 00:54:20 -0300

    Adicionado tratamento de exceção para delete (integridade referencial)

---
+++ Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sales_ASPNET_Core.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
