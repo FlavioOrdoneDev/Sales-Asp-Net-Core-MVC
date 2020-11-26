cf2404d3a1f71334c13823fbe58412b7e8145fa7
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-26 00:23:28 -0300

    Adicionado a validacao do ModelState na SellerController

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -40,6 +40,13 @@ namespace Sales_ASPNET_Core.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                List<Department> departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }                
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -78,6 +85,13 @@ namespace Sales_ASPNET_Core.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                List<Department> departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
 
